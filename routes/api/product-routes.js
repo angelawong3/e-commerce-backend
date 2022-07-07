a290b749ab1c1945e62db1cbb173c1fb274dde16
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-08 00:00:59 +0100

    complete del request for product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,15 +116,33 @@ router.put("/:id", (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((updatedProductTags) =>
+      res.json({ success: true }, updatedProductTags)
+    )
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
